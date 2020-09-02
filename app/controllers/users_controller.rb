d84772b09f5a26dd5d4c640d4ca13c8295917dda
Ridwan Abiola <ricklick262@gmail.com>
2020-09-02 10:34:34 +0100

    test, authentication, user table setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def new
+    attr_accessor :name, :email, :password
+      def initialize(attributes = {})
+      @name  = attributes[:name]
+      @email = attributes[:email]
+      @password = attributes[:password]
+      end
   end
 end
